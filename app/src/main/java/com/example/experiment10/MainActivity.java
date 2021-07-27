731ac3750253190fa097121e7a5e13146092900f
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-07-27 22:10:47 +0530

    completed

--- app/src/main/java/com/example/experiment10/MainActivity.java
+++ app/src/main/java/com/example/experiment10/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.experiment10;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int color=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +19,32 @@ public class MainActivity extends AppCompatActivity {
         final SeekBar FontSize = findViewById(R.id.FontSize);
         FontSize.setMax(60);
         final Button FontColor = findViewById(R.id.FontColor);
+
+        FontColor.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                switch (color) {
+                    case 1:
+                        textEle.setTextColor(Color.RED); break;
+                    case 2:
+                        textEle.setTextColor(Color.GREEN); break;
+                    case 3:
+                        textEle.setTextColor(Color.BLUE); break;
+                    case 4:
+                        textEle.setTextColor(Color.CYAN); break;
+                    case 5:
+                        textEle.setTextColor(Color.YELLOW); break;
+                    case 6:
+                        textEle.setTextColor(Color.MAGENTA); break;
+                }
+                color++;
+                if(color>=7){
+                    color=1;
+                }
+
+            }
+        });
+
+
 
 //       seekbar tracking
         FontSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
