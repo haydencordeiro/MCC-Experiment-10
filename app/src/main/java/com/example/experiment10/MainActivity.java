c6c31ef1c76f225a3ac00119beb8dc409d9320fe
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-07-27 21:56:44 +0530

    implemented ui

---
+++ app/src/main/java/com/example/experiment10/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.experiment10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textEle = findViewById(R.id.textInp);
+
+        final SeekBar FontSize = findViewById(R.id.FontSize);
+        final Button FontColor = findViewById(R.id.FontColor);
+
+//       seekbar tracking
+        FontSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textEle.setText(String.valueOf(progressChangedValue));
+            }
+        });
+
+    }
+
+    }
+
+
+
